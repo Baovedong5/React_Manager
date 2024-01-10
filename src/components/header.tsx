06a76b8fc3126d4b562a0e5596fe7af232de5732
Phuong <lephuong10142002@gmail.com>
2024-01-10 15:20:25 +0700

    add redux persist

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,43 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import { useAppSelector } from "../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import Form from "react-bootstrap/Form";
+import { changeMode } from "../redux/app/appSlice";
+import { useEffect } from "react";
 
 function Header() {
   const users = useAppSelector((state) => state.user.listUser);
+  const mode = useAppSelector((state) => state.app.mode);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (body) body.setAttribute("data-bs-theme", mode);
+  }, [mode]);
+
   return (
-    <Navbar className="bg-body-tertiary">
+    <Navbar className="bg-body-tertiary" data-bs-theme={mode}>
       <Container>
         <Navbar.Brand href="#home">
           Phuong with Redux {users.length}
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
+          <Form.Check
+            defaultChecked={mode === "light" ? false : true}
+            onChange={(e) =>
+              dispatch(changeMode(e.target.checked === true ? "dark" : "light"))
+            }
+            type="switch"
+            id="custom-switch"
+            label={
+              mode === "light" ? (
+                <Navbar.Text>Light mode</Navbar.Text>
+              ) : (
+                <Navbar.Text>Dark mode</Navbar.Text>
+              )
+            }
+          />
         </Navbar.Collapse>
       </Container>
     </Navbar>
